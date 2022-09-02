7f228bfb0be998032152fed14bbd846b505c814c
heze <ze_he@foxitsoftware.cn>
2022-09-02 18:18:29 +0800

    update Post

--- blog/src/main/java/com/hz/blog/entity/Post.java
+++ blog/src/main/java/com/hz/blog/entity/Post.java
@@ -13,18 +13,44 @@ import java.math.BigInteger;
 @AllArgsConstructor                //有参构造
 @Configuration
 public class Post {
+
     private BigInteger id;
+
     private BigInteger authorId;
+
+    //渠道id
     private Integer channelId;
+
+    //评论数
     private Integer comments;
+
+    //创建时间
     private DateTime created;
+
+    //喜爱数
     private Integer favors;
+
+    //
     private Integer featured;
+
+    //状态
     private Integer status;
+
+    //概要
     private String summary;
+
+    //标签
     private String tags;
+
+    //缩略图
     private String thumbnail;
+
+    //标题
     private String title;
+
+    //查看次数
     private Integer views;
+
+    //权重
     private Integer weight;
 }
