d76a81929df39ea79f295c10cf9f671e35cb882e
heze <ze_he@foxitsoftware.cn>
2022-12-09 14:46:02 +0800

    fix test4

--- blog/src/main/java/com/hz/blog/controller/blog/PostController.java
+++ blog/src/main/java/com/hz/blog/controller/blog/PostController.java
@@ -91,6 +91,7 @@ public class PostController extends BaseController {
         System.out.println("test1");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
 
         //String principal = (String) SecurityUtils.getSubject().getPrincipal();
         //Claims claims = jwtUtil.parseJWT(principal);
